067f9c744eeefe4aed62b7bdd6278a03ea70cf54
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-18 21:00:48 -0300

    add display name in methods

--- 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/demoproject/DemoUtilsTest.java
+++ 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/demoproject/DemoUtilsTest.java
@@ -11,8 +11,29 @@ class DemoUtilsTest {
     @BeforeEach
     void setupBeforeEach() {
         demoUtils = new DemoUtils();
-        System.out.println("@BeforeEach executes before the execution of each test method");
     }
+
+    @Test
+    // alterando o nome do metodo para um texto
+    @DisplayName("Equals and Not Equals")
+    void test_EqualsAndNotEquals() {
+
+        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
+
+        String str1 = null;
+        String str2 = "luv2code";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should be not null");
+    }
+
+    /*
 
     @AfterEach
     void tearDownAfterEach() {
@@ -28,27 +49,6 @@ class DemoUtilsTest {
     @AfterAll
     static void tearDownAfterAll() {
         System.out.println("@AfterAll executes only once before all test methods execution in the class");
-    }
-
-    @Test
-    void testEqualsAndNotEquals() {
-
-        System.out.println("Running test: testEqualsAndNotEquals");
-
-        assertEquals(6, demoUtils.add(2, 4), "2+4 must be 6");
-        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
-    }
-
-    @Test
-    void testNullAndNotNull() {
-
-        System.out.println("Running test: testNullAndNotNull");
-
-        String str1 = null;
-        String str2 = "luv2code";
-
-        assertNull(demoUtils.checkNull(str1), "Object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "Object should be not null");
-    }
+    } */
 
 }
