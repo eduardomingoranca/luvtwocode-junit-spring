6f4c37ba0e77dcac007c6ba020959054e72e9e61
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-21 20:36:48 -0300

    add conditional tests - enabled if environment variable and enabled if system property

--- 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/demoproject/ConditionalTest.java
+++ 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/demoproject/ConditionalTest.java
@@ -2,9 +2,7 @@ package com.brazil.luvtwocode.demoproject;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledForJreRange;
-import org.junit.jupiter.api.condition.EnabledOnJre;
-import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.*;
 
 import static org.junit.jupiter.api.condition.JRE.*;
 import static org.junit.jupiter.api.condition.OS.*;
@@ -62,6 +60,18 @@ class ConditionalTest {
     @Test
     @EnabledForJreRange(min=JAVA_9)
     void testForOnlyForJavaRangeMin() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "LUV_TWO_CODE_ENV", matches = "DEV")
+    void testOnlyForDevEnvironment() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "LUV_TWO_CODE_SYS_PROP", matches = "CI_CD_DEPLOY")
+    void testOnlyForSystemProperty() {
         // execute method and perform asserts
     }
 
