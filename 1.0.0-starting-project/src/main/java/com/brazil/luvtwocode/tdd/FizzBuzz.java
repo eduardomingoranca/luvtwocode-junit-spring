ce659eaeffc2fd5cd5658d07326345bb5a6cf43f
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-22 14:42:34 -0300

    refactoring fizzbuzz class

--- 1.0.0-starting-project/src/main/java/com/brazil/luvtwocode/tdd/FizzBuzz.java
+++ 1.0.0-starting-project/src/main/java/com/brazil/luvtwocode/tdd/FizzBuzz.java
@@ -3,6 +3,25 @@ package com.brazil.luvtwocode.tdd;
 public class FizzBuzz {
 
     public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+
+        if (result.toString().isEmpty()) {
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+
+    /*
+    public static String compute(int i) {
 
         // If number is divisible by 3 or 5, print FizzBuzz
         if ((i % 3 == 0) && (i % 5 == 0)) {
@@ -21,5 +40,6 @@ public class FizzBuzz {
             return Integer.toString(i);
         }
     }
+    */
 
 }
