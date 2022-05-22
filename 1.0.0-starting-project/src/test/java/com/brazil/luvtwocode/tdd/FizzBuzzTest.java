ccf7feb167ec43d031423693219c49fc7c44b38d
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-22 14:17:19 -0300

    add fizbuzz class and divisible tests

--- 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/tdd/FizzBuzzTest.java
+++ 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/tdd/FizzBuzzTest.java
@@ -2,13 +2,13 @@ package com.brazil.luvtwocode.tdd;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static com.brazil.luvtwocode.tdd.FizzBuzz.compute;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FizzBuzzTest {
 
     // If number is divisible by 3, print Fizz
-    // If number is divisible by 5, print Buzz
     // If number is divisible by 3 or 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
 
@@ -16,7 +16,20 @@ class FizzBuzzTest {
     @Test
     @Order(1)
     void testForDivisibleByThree() {
-        fail("fail");
+        String expected = "Fizz";
+
+        assertEquals(expected, compute(3), "Should return Fizz");
+    }
+
+    // If number is divisible by 5, print Buzz
+
+    @DisplayName("Divisible by Five")
+    @Test
+    @Order(2)
+    void testForDivisibleByFive() {
+        String expected = "Buzz";
+
+        assertEquals(expected, compute(5), "Should return Buzz");
     }
 
 }
