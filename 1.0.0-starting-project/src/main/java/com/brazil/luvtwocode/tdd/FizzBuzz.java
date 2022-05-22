f44dda9e3320c43fb145d0207b9db30bdf2cd3ec
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-22 14:30:36 -0300

    add divisible tests

--- 1.0.0-starting-project/src/main/java/com/brazil/luvtwocode/tdd/FizzBuzz.java
+++ 1.0.0-starting-project/src/main/java/com/brazil/luvtwocode/tdd/FizzBuzz.java
@@ -2,19 +2,24 @@ package com.brazil.luvtwocode.tdd;
 
 public class FizzBuzz {
 
-    // If number is divisible by 3, print Fizz
-    // If number is divisible by 5, print Buzz
-    // If number is divisible by 3 or 5, print FizzBuzz
-    // If number is NOT divisible by 3 or 5, then print the number
-
     public static String compute(int i) {
-        if (i % 3 == 0) {
+
+        // If number is divisible by 3 or 5, print FizzBuzz
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            return "FizzBuzz";
+        }
+        // If number is divisible by 3, print Fizz
+        else if (i % 3 == 0) {
             return "Fizz";
         }
+        // If number is divisible by 5, print Buzz
         else if (i % 5 == 0) {
             return "Buzz";
         }
-        return null;
+        // If number is NOT divisible by 3 or 5, then print the number
+        else {
+            return Integer.toString(i);
+        }
     }
 
 }
--- 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/tdd/FizzBuzzTest.java
+++ 1.0.0-starting-project/src/test/java/com/brazil/luvtwocode/tdd/FizzBuzzTest.java
@@ -9,8 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzTest {
 
     // If number is divisible by 3, print Fizz
-    // If number is divisible by 3 or 5, print FizzBuzz
-    // If number is NOT divisible by 3 or 5, then print the number
 
     @DisplayName("Divisible by Three")
     @Test
@@ -30,6 +28,28 @@ class FizzBuzzTest {
         String expected = "Buzz";
 
         assertEquals(expected, compute(5), "Should return Buzz");
+    }
+
+    // If number is divisible by 3 and 5, print FizzBuzz
+
+    @DisplayName("Divisible by Three and Five")
+    @Test
+    @Order(3)
+    void testForDivisibleByThreeAndFive() {
+        String expected = "FizzBuzz";
+
+        assertEquals(expected, compute(15), "Should return FizzBuzz");
+    }
+
+    // If number is NOT divisible by 3 or 5, then print the number
+
+    @DisplayName("Not Divisible by Three or Five")
+    @Test
+    @Order(4)
+    void testForNotDivisibleByThreeOrFive() {
+        String expected = "1";
+
+        assertEquals(expected, compute(1), "Should return 1");
     }
 
 }
