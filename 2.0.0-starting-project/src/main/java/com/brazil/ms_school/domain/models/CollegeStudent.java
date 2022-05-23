7155f65b72be533b88e5965aca288e1d4b42f80e
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-23 18:27:16 -0300

    add classes setup

---
+++ 2.0.0-starting-project/src/main/java/com/brazil/ms_school/domain/models/CollegeStudent.java
@@ -0,0 +1,35 @@
+package com.brazil.ms_school.domain.models;
+
+import com.brazil.ms_school.domain.ports.StudentPort;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class CollegeStudent implements StudentPort {
+
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+
+    public CollegeStudent(String firstname, String lastname, String emailAddress) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String studentInformation() {
+        return getFullName() + " " + getEmailAddress();
+    }
+
+    @Override
+    public String getFullName() {
+        return getFirstname() + " " + getLastname();
+    }
+
+}
---
+++ 2.0.0-starting-project/src/main/java/com/brazil/ms_school/domain/ports/StudentPort.java
@@ -0,0 +1,7 @@
+package com.brazil.ms_school.domain.ports;
+
+public interface StudentPort {
+
+    String studentInformation();
+    String getFullName();
+}
