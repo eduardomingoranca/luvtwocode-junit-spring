3c6f33ae4b2101f0d2fa6cb377156e27b1a997bb
eduardomingoranca <eduardomingoranca@gmail.com>
2022-05-25 23:52:13 -0300

    add reflection test utils - get private field and invoke private method

--- 2.20-starting-project/src/test/java/com/brazil/ms_school/ReflectionTestUtilsTest.java
+++ 2.20-starting-project/src/test/java/com/brazil/ms_school/ReflectionTestUtilsTest.java
@@ -4,6 +4,7 @@ package com.brazil.ms_school;
 import com.brazil.ms_school.app.core.StudentGradesCore;
 import com.brazil.ms_school.app.model.CollegeStudent;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -12,6 +13,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 import java.util.ArrayList;
 
 import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
   reflections utils -> acessa metodos e atributos/campos/variaveis privadas,
@@ -43,5 +45,16 @@ class ReflectionTestUtilsTest {
                         100.0, 85.0, 76.5, 91.75))));
     }
 
+    @Test
+    void getPrivateField() {
+        assertEquals(1, ReflectionTestUtils.getField(studentOne, "id"));
+    }
+
+    @Test
+    void invokePrivateMethod() {
+        assertEquals("Lon 1",
+                ReflectionTestUtils.invokeMethod(studentOne, "getFirstNameAndId"),
+                "Fail private method not call");
+    }
 
 }
